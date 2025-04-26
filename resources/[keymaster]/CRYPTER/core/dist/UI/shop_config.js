97f236175f2da79fef9e0001299b8f8618f04e19
Néo <116284443+NEOJIMM@users.noreply.github.com>
2025-04-26 22:45:11 +0200

    update

--- resources/[keymaster]/CRYPTER/core/dist/UI/shop_config.js
+++
@@ -1,8 +0,0 @@
-// /** Don't change */
-// var Config = new Object();
-// /** Don't change */
-
-// /** Keys that will close the menu */
-// //Config.closeKeys = [113, 27, 90]; 
-// Config.closeKeys = [27]; 
-// /** Keys that will close the menu */
