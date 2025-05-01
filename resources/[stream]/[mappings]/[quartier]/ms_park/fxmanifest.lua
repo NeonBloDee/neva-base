cd791c099c4e3e4dd714eeeb79194e338097cd19
Néo <neojim.dev@gmail.com>
2025-05-01 12:22:21 +0200

    mappings part4

---
+++ resources/[stream]/[mappings]/[quartier]/ms_park/fxmanifest.lua
@@ -0,0 +1,33 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+this_is_a_map 'yes'
+
+
+
+data_file 'DLC_ITYP_REQUEST' 'stream/kg_mosleypark_land_data.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/kg_mosleypark_landfence_data.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/kg_mosleypark_archway_data.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/kg_mosleypark_walkway_data.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/kg_mosleypark_props_data.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/kg_mosleypark_veg_01_data.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/kg_mosleypark_fountain_data.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/kg_mosleypark_playground_data.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/kg_mosleypark_gazebo_data.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/kg_mosleypark_stepstones_data.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/kg_mosleypark_pole_data.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/kg_mosleypark_bollard_data.ytyp'
+
+
+dependency '/assetpacks'
