347ab095f8507132d23073813556440fe6597a85
Néo <neojim.dev@gmail.com>
2025-05-01 10:20:53 +0200

    mappings part1

---
+++ resources/[stream]/[mappings]/[garages]/Octa_Cyp_Garage/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map "yes"
+
+data_file 'TIMECYCLEMOD_FILE' 'oct_garage_timecycle.xml'
+
+files{
+    'oct_garage_timecycle.xml'
+}
+dependency '/assetpacks'
---
+++ resources/[stream]/[mappings]/[garages]/ss_garage/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+this_is_a_map 'yes'
+
+
+data_file 'DLC_ITYP_REQUEST' 'stream/kg_autogarage_curb_data.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/kg_autogarage_data.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/kg_autogarage_land_data.ytyp'
+
+
+
+dependency '/assetpacks'
