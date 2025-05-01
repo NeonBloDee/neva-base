44eabefadd2054c507d275aa8d81d55a990851aa
Néo <neojim.dev@gmail.com>
2025-05-01 10:47:55 +0200

    mappings part2

---
+++ resources/[stream]/[mappings]/[fdo]/mrpd/fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'DTPL* <info@dtpl.cz>'
+description 'Vinewood One'
+version '1.1.1'
+dependency '/assetpacks'
---
+++ resources/[stream]/[mappings]/[ltd]/PaletoBayLTD/fxmanifest.lua
@@ -0,0 +1,24 @@
+-- LEAKED BY MIKE
+
+fx_version 'bodacious'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/reh_prop_reh_desk_comp_01a.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/bkr_int_01_bike_parts_006.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/v_ind_rc_bench.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'lr_supermod_banners2.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'ss1_06_surface_banners.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'lr_supermod_banners004.ytyp'
+
+files {
+    'stream/**/*.ydr',
+    'stream/**/*.ybn',
+    'stream/**/*.ytyp',
+    'stream/**/*.ymf',
+    'stream/**/*.ymap',
+    'stream/**/*.ycd',
+}
+
+dependency '/assetpacks'
