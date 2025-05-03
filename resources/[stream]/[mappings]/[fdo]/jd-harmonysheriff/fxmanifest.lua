4a684f730c665c4a67e1e073e2db341a5d5bd88a
Néo <neojim.dev@gmail.com>
2025-05-03 20:47:08 +0200

    update mappings

---
+++ resources/[stream]/[mappings]/[fdo]/jd-harmonysheriff/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'John Chin'
+description 'Harmony Sheriff MLO'
+version '1.0.0'
+
+file "sp_manifest.ymt"
+data_file "SCENARIO_POINTS_OVERRIDE_PSO_FILE" "sp_manifest.ymt"
