426102fb0e23800e9e00b497b21632c90137d8bc
Néo <neojim.dev@gmail.com>
2025-05-06 15:23:33 +0200

    update

--- resources/[lua]/wItem/cl_item.lua
+++ resources/[lua]/wItem/cl_item.lua
@@ -13,15 +13,36 @@ AddEventHandler('wItem:repairNearestVehicle', function()
     local vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 5.0, 0, 71)
 
     if DoesEntityExist(vehicle) then
-        TaskStartScenarioInPlace(playerPed, 'PROP_HUMAN_BUM_BIN', 0, true)
-        SetVehicleFixed(vehicle)
-        SetVehicleDeformationFixed(vehicle)
-        SetVehicleUndriveable(vehicle, false)
-        ClearPedTasksImmediately(playerPed)
-        ESX.ShowNotification('~g~Vehicle reparé ! ')
-        TriggerServerEvent('wItem:RepareOK')
+        local dict = "mini@repair"
+        RequestAnimDict(dict)
+        while not HasAnimDictLoaded(dict) do
+            Wait(100)
+        end
+        
+        FreezeEntityPosition(playerPed, true)
+        TaskPlayAnim(playerPed, dict, "fixing_a_player", 8.0, -8.0, -1, 49, 0, false, false, false)
+        
+        if lib.progressBar({
+            duration = 8000,
+            label = 'Réparation en cours...',
+            useWhileDead = false,
+            canCancel = false,
+            disable = {
+                car = true,
+                move = true,
+                combat = true
+            },
+        }) then
+            SetVehicleFixed(vehicle)
+            SetVehicleDeformationFixed(vehicle)
+            SetVehicleUndriveable(vehicle, false)
+            ClearPedTasksImmediately(playerPed)
+            FreezeEntityPosition(playerPed, false)
+            ESX.ShowNotification('~g~Véhicule réparé avec succès!')
+            TriggerServerEvent('wItem:RepareOK')
+        end
     else
-        ESX.ShowNotification('~r~Pas de véhicule autour !')
+        ESX.ShowNotification('~r~Pas de véhicule à proximité!')
     end
 end)
 
@@ -33,30 +54,80 @@ AddEventHandler('wItem:cleanNearestVehicle', function()
     local vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 5.0, 0, 71)
 
     if DoesEntityExist(vehicle) then
-        TaskStartScenarioInPlace(playerPed, 'WORLD_HUMAN_MAID_CLEAN', 0, true)
-        SetVehicleDirtLevel(vehicle, 0)
-        ClearPedTasksImmediately(playerPed)
-        ESX.ShowNotification('~g~Vehicle Nettoyé !')
-        TriggerServerEvent('wItem:CleanOK')
+        local dict = "timetable@floyd@clean_kitchen@base"
+        RequestAnimDict(dict)
+        while not HasAnimDictLoaded(dict) do
+            Wait(100)
+        end
+        
+        FreezeEntityPosition(playerPed, true)
+        TaskPlayAnim(playerPed, dict, "base", 8.0, -8.0, -1, 49, 0, false, false, false)
+        
+        if lib.progressBar({
+            duration = 6000,
+            label = 'Nettoyage en cours...',
+            useWhileDead = false,
+            canCancel = false,
+            disable = {
+                car = true,
+                move = true,
+                combat = true
+            },
+        }) then
+            SetVehicleDirtLevel(vehicle, 0)
+            ClearPedTasksImmediately(playerPed)
+            FreezeEntityPosition(playerPed, false)
+            ESX.ShowNotification('~g~Véhicule nettoyé avec succès!')
+            TriggerServerEvent('wItem:CleanOK')
+        end
     else
-        ESX.ShowNotification('~r~Pas de véhicule autour !')
+        ESX.ShowNotification('~r~Pas de véhicule à proximité!')
     end
 end)
 
 RegisterNetEvent('wItem:startHealAnimation')
-AddEventHandler('wItem:startHealAnimation', function()
+AddEventHandler('wItem:startHealAnimation', function(isMedikit)
     local playerPed = PlayerPedId()
-    FreezeEntityPosition(playerPed, true)
-    RequestAnimDict("anim@amb@nightclub@peds@")
-    while not HasAnimDictLoaded("anim@amb@nightclub@peds@") do
+    local dict = "anim@amb@clubhouse@tutorial@bkr_tut_ig3@"
+    local anim = "machinic_loop_mechandplayer"
+    
+    RequestAnimDict(dict)
+    while not HasAnimDictLoaded(dict) do
         Wait(100)
     end
     
-    TaskPlayAnim(playerPed, "anim@amb@nightclub@peds@", "amb_medic_standing_timeofdeath_base", 8.0, -8.0, -1, 49, 0, false, false, false)
-
-
-    FreezeEntityPosition(playerPed, false)
+    FreezeEntityPosition(playerPed, true)
+    TaskPlayAnim(playerPed, dict, anim, 8.0, -8.0, -1, 1, 0, false, false, false)
+    
+    local duration = isMedikit and 8000 or 4000
+    local label = isMedikit and 'Application du kit de soin...' or 'Application du bandage...'
+    
+    if lib.progressBar({
+        duration = duration,
+        label = label,
+        useWhileDead = false,
+        canCancel = false,
+        disable = {
+            car = true,
+            move = true,
+            combat = true
+        },
+    }) then
+        -- Progress complete
+        if isMedikit then
+            -- Update health directly on client side
+            SetEntityHealth(playerPed, GetEntityMaxHealth(playerPed))
+        else
+            -- Add 20% of max health
+            local maxHealth = GetEntityMaxHealth(playerPed)
+            local currentHealth = GetEntityHealth(playerPed)
+            local newHealth = math.min(currentHealth + (maxHealth * 0.20), maxHealth)
+            SetEntityHealth(playerPed, newHealth)
+        end
+    end
+    
     ClearPedTasks(playerPed)
+    FreezeEntityPosition(playerPed, false)
 end)
 
 RegisterCommand('kill', function()
--- resources/[lua]/wItem/sv_item.lua
+++ resources/[lua]/wItem/sv_item.lua
@@ -8,10 +8,8 @@ end)
 
 RegisterServerEvent("wItem:RepareOK")
 AddEventHandler("wItem:RepareOK", function()
-
     local xPlayer = ESX.GetPlayerFromId(source)
     xPlayer.removeInventoryItem('repairkit', 1)
-
 end)
 
 ESX.RegisterUsableItem('kitcarro', function(source)
@@ -21,29 +19,17 @@ end)
 
 RegisterServerEvent("wItem:CleanOK")
 AddEventHandler("wItem:CleanOK", function()
-
     local xPlayer = ESX.GetPlayerFromId(source)
     xPlayer.removeInventoryItem('kitcarro', 1)
-
 end)
 
 ESX.RegisterUsableItem('bandage', function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
-    TriggerEvent('wItem:useBandage', source)
-    print('je suis la')
-end)
-
-RegisterServerEvent('wItem:useBandage')
-AddEventHandler('wItem:useBandage', function(source)
-    local xPlayer = ESX.GetPlayerFromId(source)
-
+    
     if xPlayer.getInventoryItem('bandage').count > 0 then
-        local maxHealth = GetEntityMaxHealth(GetPlayerPed(source))
-        local healAmount = math.floor(maxHealth * 0.20)
-        TriggerEvent('esx_status:add', source, 'health', healAmount)
+        TriggerClientEvent('wItem:startHealAnimation', source, false)
         xPlayer.removeInventoryItem('bandage', 1)
-        TriggerClientEvent('wItem:startHealAnimation', source)
-        TriggerClientEvent('esx:showNotification', source, 'Vous avez utilisé un bandage et récupéré une partie de votre santé.')
+        TriggerClientEvent('esx:showNotification', source, 'Vous avez utilisé un bandage.')
     else
         TriggerClientEvent('esx:showNotification', source, 'Vous n\'avez pas de bandage.')
     end
@@ -51,19 +37,13 @@ end)
 
 ESX.RegisterUsableItem('medikit', function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
-    TriggerEvent('wItem:useMedikit', source)
-end)
-
-RegisterServerEvent('wItem:useMedikit')
-AddEventHandler('wItem:useMedikit', function(source)
-    local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-
-    TriggerClientEvent('wItem:startHealAnimation', _source)
-
-    TriggerEvent('esx_status:set', _source, 'health', 100)
-    xPlayer.removeInventoryItem('medikit', 1)
-
-    TriggerClientEvent('esx:showNotification', _source, 'Vous avez utilisé un medikit et récupéré toute votre santé.')
+    
+    if xPlayer.getInventoryItem('medikit').count > 0 then
+        TriggerClientEvent('wItem:startHealAnimation', source, true)
+        xPlayer.removeInventoryItem('medikit', 1)
+        TriggerClientEvent('esx:showNotification', source, 'Vous avez utilisé un medikit.')
+    else
+        TriggerClientEvent('esx:showNotification', source, 'Vous n\'avez pas de medikit.')
+    end
 end)
 
