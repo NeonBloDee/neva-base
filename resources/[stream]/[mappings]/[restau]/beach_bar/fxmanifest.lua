1a3f3a42fb1649f4d1d150bb71bb259cdae3a828
Néo <116284443+NEOJIMM@users.noreply.github.com>
2025-05-22 14:11:46 +0200

    update maps6

---
+++ resources/[stream]/[mappings]/[restau]/beach_bar/fxmanifest.lua
@@ -0,0 +1,11 @@
+
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+author 'Nothin Studio'
+description 'Vespucci Beach Bar'
+version '1.0.0'
+dependency '/assetpacks'
+dependency '/assetpacks'
---
+++ resources/[stream]/[mappings]/[restau]/unclejust_paletobar_mojito_inn/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "bodacious"
+game "gta5"
+
+this_is_a_map "yes"
+
+lua54 'yes'
+
+data_file('DLC_ITYP_REQUEST')('stream/uj_bar_props.ytyp')
+data_file('DLC_ITYP_REQUEST')('stream/uj_int_bar.ytyp')
+data_file('DLC_ITYP_REQUEST')('stream/uj_int_kh.ytyp')
+data_file('DLC_ITYP_REQUEST')('stream/uj_int_st.ytyp')
+dependency '/assetpacks'
